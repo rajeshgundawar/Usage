8f422d5efe4055e697b3b77bd0d979335416a679
Rajesh Gundawar <rajeshgundawar@Rajeshs-MacBook-Pro.local>
2018-06-05 17:51:58 +0530

    changing code

--- scripts/usage-script.sh
+++ scripts/usage-script.sh
@@ -27,7 +27,7 @@ else
 aws --version
 cat <<EOF > ./message.json
             {
-              "Data": "From: rajesh.gundawar@techolution.com\nTo: $TO\nSubject: $subject Report\nMIME-Version: 1.0\nContent-type: Multipart/Mixed; boundary=\"NextPart\"\n\n--NextPart\nContent-Type: text/plain\n\n$body.\n\n--NextPart\nContent-Type: text/csv;\nContent-Disposition: attachment; Content-Transfer-Encoding: base64;\n--NextPart--"
+              "Data": "From: rajesh.gundawar@techolution.com\nTo: $TO\nSubject: $subject Report\nMIME-Version: 1.0\nContent-type: Multipart/Mixed; boundary=\"NextPart\"\n\n--NextPart\nContent-Type: text/plain\n\n$body.\n\n--NextPart\nContent-Type: text/csv;\nContent-Transfer-Encoding: base64;\n--NextPart--"
             }
 EOF
 aws ses send-raw-email --raw-message file://message.json
